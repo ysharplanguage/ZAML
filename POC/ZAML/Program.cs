c61633fa5f7533f38c5a562215236cfbd6ee110b
CJJ <ysharp.design@gmail.com>
2025-02-20 02:27:27 -0500

    Support multiline literals, int64, decimal

--- POC/ZAML/Program.cs
+++ POC/ZAML/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 namespace ZAML
 {
     internal class Program
@@ -11,22 +12,20 @@ namespace ZAML
         {
             internal sealed class LineReader
             {
-                private readonly string[] lines;
-                private int at = -1;
+                private readonly string[] lines; private int at = -1;
                 internal LineReader(string[] lines) => this.lines = lines;
-                internal string Next() { int tail; while (++at < lines.Length && (string.Equals(lines[at].Trim(), string.Empty) || string.Equals((lines[at] = (tail = lines[at].LastIndexOf("//")) >= 0 && lines[at].LastIndexOf('"') < tail ? lines[at].Substring(0, tail).TrimEnd() : lines[at]).Trim(), string.Empty))) ; return at < lines.Length ? lines[at] : null; }
+                internal string Next() { int tail; while (++at < lines.Length && (string.Equals(lines[at].Trim(), string.Empty) || string.Equals((lines[at] = (tail = lines[at].IndexOf("//")) >= 0 ? lines[at].Substring(0, tail).TrimEnd() : lines[at]).Trim(), string.Empty))) ; return at < lines.Length ? lines[at] : null; }
                 internal int LineNo => at + 1;
             }
             private readonly int indent;
+            private static readonly Regex Literals = new Regex("\"(\\\\\"|[^\"])*\"", RegexOptions.Compiled);
             private static KeyValuePair<string, object> NewKey(string name) => new KeyValuePair<string, object>(name, null);
             private static object EmptyArray() => new object[0];
             private static object EmptyObject() => new Dictionary<string, object>();
             private static (object, string) DoParse(LineReader reader, int indent, int at = 0, string nextLine = null)
             {
                 static int IndentOf(string line) { int count = 0, c = -1; while (++c < line.Length && char.IsWhiteSpace(line[c])) count++; return count; }
-                var parse = new List<object>();
-                var hash = false;
-                string line;
+                var parse = new List<object>(); var hash = false; string line;
                 while (true)
                 {
                     if ((line = nextLine ?? reader.Next()) == null) return (parse.ToArray(), string.Empty);
@@ -81,13 +80,12 @@ namespace ZAML
                                     }
                                     else
                                     {
-                                        if (long.TryParse(value, out var n))
-                                        {
-                                            parse.Add(n);
-                                        }
+                                        if (int.TryParse(value, out var i32)) parse.Add(i32);
+                                        else if (long.TryParse(value, out var i64)) parse.Add(i64);
+                                        else if (decimal.TryParse(value, out var d)) parse.Add(d);
                                         else if (value.StartsWith('"') && value.EndsWith('"'))
                                         {
-                                            parse.Add(value.Substring(1, value.Length - 2));
+                                            parse.Add(value.Substring(1, value.Length - 2).Replace("\\n", "\n").Replace("\\\"", "\""));
                                         }
                                         else
                                         {
@@ -131,13 +129,12 @@ namespace ZAML
                         }
                         else
                         {
-                            if (long.TryParse(value, out var n))
-                            {
-                                parse.Add(n);
-                            }
+                            if (int.TryParse(value, out var i32)) parse.Add(i32);
+                            else if (long.TryParse(value, out var i64)) parse.Add(i64);
+                            else if (decimal.TryParse(value, out var d)) parse.Add(d);
                             else if (value.StartsWith('"') && value.EndsWith('"'))
                             {
-                                parse.Add(value.Substring(1, value.Length - 2));
+                                parse.Add(value.Substring(1, value.Length - 2).Replace("\\n", "\n").Replace("\\\"", "\""));
                             }
                             else
                             {
@@ -175,13 +172,10 @@ namespace ZAML
             public OffsideParser(int indent = 0) => this.indent = indent > 1 ? indent : 2;
             public object Parse(string input)
             {
+                static string Replacer(Match match) => match.Value.Replace("\n", "\\n");
                 static bool IsValid(object o) =>
-                    o == null ||
-                    (o is Dictionary<string, object> d && d.Values.All(IsValid)) ||
-                    (o is object[] a && a.All(IsValid)) ||
-                    o is string ||
-                    o is long ||
-                    o is bool;
+                    o == null || (o is Dictionary<string, object> d && d.Values.All(IsValid)) || (o is object[] a && a.All(IsValid)) ||
+                    o is string || o is decimal || o is long || o is int || o is bool;
                 static object Normalize(object o)
                 {
                     if (o is object[] a && a.Length > 0)
@@ -228,7 +222,7 @@ namespace ZAML
                     }
                     return o;
                 }
-                var reader = new LineReader(input.ReplaceLineEndings("\n").Split('\n'));
+                var reader = new LineReader(Literals.Replace(input.ReplaceLineEndings("\n"), Replacer).Split('\n'));
                 var eaten = DoParse(reader, indent);
                 var array = (object[])eaten.Item1;
                 if (array.Length > 0 && ReferenceEquals(array[array.Length - 1], string.Empty)) Array.Resize(ref array, array.Length - 1);
@@ -252,6 +246,7 @@ namespace ZAML
     ""zaml"": [
       ""Zero (Almost!) Markup Language"",
       ""more consistent than YAML"",
+      ""even better for configuration...\n... and,\n    well... \u0022yeah!\u0022...\n other things"",
       ""smarter arrays (of arrays of arrays...)""
     ],
     ""nice_ZAML_array_of_array"": [
@@ -263,10 +258,10 @@ namespace ZAML
           ""id"": ""I am an object, somewhat buried ;^)"",
           ""value"": ""whatever else""
         },
-        ""d""
+        ""e""
       ],
-      ""foo bar"",
-      ""e"",
+      "" foo...\n    ... and bar..."",
+      ""f"",
       ""g e e""
     ],
     ""object"": {
@@ -280,7 +275,13 @@ namespace ZAML
         ""boolean"": true
       },
       {
-        ""integer"": 1
+        ""integer"": -65537
+      },
+      {
+        ""long"": 136129581883
+      },
+      {
+        ""decimal"": 3.1416
       }
     ],
     ""alias"": {
@@ -291,9 +292,8 @@ namespace ZAML
             var input = File.ReadAllText("ZAML-Test-Me.txt"/*args[0]*/);
             var parse = new OffsideParser(3).Parse(input);
             var json = JsonSerializer.Serialize(parse, new JsonSerializerOptions { WriteIndented = true });
-            Console.WriteLine(json);
             System.Diagnostics.Debug.Assert(json == expected_json);
-            Console.WriteLine("The end");
+            Console.WriteLine(json);
             Console.ReadKey(true);
         }
     }
